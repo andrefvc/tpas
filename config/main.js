b0f296befa764f4ff7676ab19901b46b10c8f5eb
Andre Carneiro <uia53268@contiwan.com>
2018-02-02 09:43:41 +0000

    prod

--- config/main.js
+++ config/main.js
@@ -3,18 +3,17 @@ module.exports = {
 'secret': 'ilovescotchyscotch',
 
 //// dev
+/*
 'facebook': {
     clientID: '105701353575403',
     clientSecret: '4932f405dd5ed47fe96f5c7fa4d47bc0',
     callbackURL: 'http://http://localhost:3000/auth/facebook/callback'
   }
-
-  //// prod
-  /*
+*/
+  //// prod  
   'facebook': {
     clientID: '415025925564529',
     clientSecret: '7ec3a0153fe31e6656250611972c45ad',
     callbackURL: 'https://tppweb.herokuapp.com/auth/facebook/callback'
   }
-  */
 };
